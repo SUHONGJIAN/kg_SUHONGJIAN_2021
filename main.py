6f56603655380bc6eceabc9d59557b7c948c689a
hongjian su <hongjiansu@hongjiandeMBP.fios-router.home>
2020-03-17 16:59:08 -0400

    Establish basic frame

---
+++ main.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def check1v1Mapping(self, s1: str, s2: str) -> bool:
+        pass
+
+class MainProcess(object):
+    def main(self):
+        pass
+
+MainProcess().main()
